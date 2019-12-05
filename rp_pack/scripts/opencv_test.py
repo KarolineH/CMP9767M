d67e5ddb0dec167873e5f9cc99d84a0b7fa1d715
KarolineH <karoline@heiwolt.de>
2019-12-05 02:49:04 +0000

    Added colour segmentation (logistic regression on manually labelled RGB images)

--- rp_pack/scripts/opencv_test.py
+++ rp_pack/scripts/opencv_test.py
@@ -2,33 +2,33 @@
 
 import rospy
 import cv2
-import matplotlib.pyplot as plt
-from numpy import mean
+import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
-class image_converter:
-
+class seeker:
     def __init__(self):
+        blink_rate = 0
     	#Interface OpenCV from ROS
         self.bridge = CvBridge()
         #Subscribe to the robot camera image stream
         self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect", Image, self.callback)
 
     def callback(self, data):
-        cv2.namedWindow("Image window")
-        cv2.namedWindow("filtered")
-        #Transfer into HSV space
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_RGB2HSV)
-        mask = cv2.inRange(hsv_img,(36, 0, 0),(86, 255,255))
-        masked_img = cv2.bitwise_and(cv_image, cv_image, mask = mask)
-        cv2.imshow("filtered", masked_img)
-        cv2.waitKey(0)
+        self.latest_image = data
+
+    def examine_image():
+        image = self.bridge.imgmsg_to_cv2(self.latest_image, "bgr8") #deserialize using opencv
+        hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_RGB2HSV) #Transfer into HSV space
+        mask = cv2.inRange(hsv_img,(36, 0, 0),(86, 255,255)) # filter everything but green pixels
+        masked_img = cv2.bitwise_and(cv_image, cv_image, mask = mask) # apply the mask to the image
 
 if __name__ == "__main__":
-	rospy.init_node('image_converter')
-	ic = image_converter()
+	rospy.init_node('Weed_seeker')
+	harry = seeker()
+    while true:
+        harry.examine_image()
 	rospy.spin()
+
 
 	destroyAllWindows()
